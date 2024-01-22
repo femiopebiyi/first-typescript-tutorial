9046d6e7238826323c68e515edf5d85bf8597403
Opebiyi Oluwafemi <opebiyibiodun10@gmail.com>
2024-01-22 09:15:42 +0100

    arrays in typescript

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,3 @@
- let sales: number = 123_456_789;
- let course: string = 'Typescript'
- let is_published: boolean = true;
- let level;
+let numbers: number[] = [1,3,3]
 
-function render(document){
-    console.log(document)
-}
+numbers.forEach(n=> n.toString)
