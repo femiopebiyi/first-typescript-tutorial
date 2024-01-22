e4e747343b8990d63f0e2e8ff025a955b6105c36
Opebiyi Oluwafemi <opebiyibiodun10@gmail.com>
2024-01-22 09:28:32 +0100

    enums

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
-let user: [number, string] = [1, 'femi']
 
-//array of fixed length
+const enum Size {
+    Small = 1,
+    Medium,
+    Large
+}
+
+let mySize: Size = Size.Medium;
+
+console.log(mySize)
